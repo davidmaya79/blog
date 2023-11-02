aed218be00ade0e96aaa251808a6f0cfc2e8bec6
david maya <davidmaya79@gmail.com>
2023-11-01 22:47:25 -0300

    nombre titulo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'MenteCuriosa',
+  author: 'Ariel',
   headerTitle: 'MenteCuriosa',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Un blog creado para pensar',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
